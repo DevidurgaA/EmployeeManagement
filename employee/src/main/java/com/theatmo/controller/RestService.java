22bc6aabe44ce6a5a2cc9ff365fdbae80e4c8e73
DevidurgaA <102131763+DevidurgaA@users.noreply.github.com>
2022-03-22 22:10:42 +0530

    changed properties file

--- employee/src/main/java/com/theatmo/controller/RestService.java
+++ employee/src/main/java/com/theatmo/controller/RestService.java
@@ -3,25 +3,32 @@ package com.theatmo.controller;
 import org.apache.cxf.BusFactory;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
+import com.theatmo.databaseconnection.DBConnection;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import java.util.Map;
+import com.theatmo.view.EmployeeDetails;
 
 /**
  * Provides address for the rest implementation
  */
-@Component
+@Component(name = "databaseconnection", immediate = true)
 public class RestService {
 
     private Server server;
+    private final EmployeeDetails EMPLOYEE_DETAILS = new EmployeeDetails();
 
     @Activate
-    public void activate() {
+    public void activate(Map<String, String> properties) {
 
         try{
+            DBConnection.databaseConnection(properties);
             JAXRSServerFactoryBean bean = new JAXRSServerFactoryBean();
 
             bean.setAddress("/employee");
@@ -30,7 +37,9 @@ public class RestService {
             bean.setServiceBean(new EmployeeRestControllerImpl());
             server = bean.create();
 
-        } catch(Exception exception) {
+            EMPLOYEE_DETAILS.selectChoice();
+
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
     }
@@ -39,6 +48,7 @@ public class RestService {
     public void deactivate() {
 
         try{
+
             if (server != null) {
                 server.destroy();
             }
